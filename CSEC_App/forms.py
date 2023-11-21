683c152f8158c5920e90c18d531a387d604e7763
felekekinfe <felekekinfe@gmail.com>
2023-11-21 06:02:48 -0500

    create MemberRegistrationForm

---
+++ CSEC_App/forms.py
@@ -0,0 +1,14 @@
+
+from django.db import models
+from .models import Members,Events
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class MembersRegistrationForm(UserCreationForm):
+    
+    class Meta:
+        model=Members
+        fields=['first_name','last_name','email','department','Roll','password1','password2']
+    
+
+
