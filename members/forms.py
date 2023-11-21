49f4f8c2eb6f384db0fddbea41a47f4b07737bec
felekekinfe <felekekinfe@gmail.com>
2023-11-21 08:04:43 -0500

    create MemberLoginForm

---
+++ members/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from CSEC_App.models import Members
+
+
+class MemberLoginForm(AuthenticationForm):
+
+    class Meta:
+        model=Members
+        fields=['email','password']
