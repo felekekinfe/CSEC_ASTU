f72518df1dfd1c9c51668c6b44cd7ba686084b8c
felekekinfe <felekekinfe@gmail.com>
2023-11-21 08:15:15 -0500

    create MembersLoginview

--- members/views.py
+++ members/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.views import LoginView
+from CSEC_App.models import Members
+from .forms import MemberLoginForm
 # Create your views here.
+
+class MembersLoginView(LoginView):
+    model=Members
+    form_class=MemberLoginForm
+
+    #add redirection
