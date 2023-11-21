557d7bfcde33b28d3194af9ddb013b9a9e267dd6
felekekinfe <felekekinfe@gmail.com>
2023-11-21 06:19:33 -0500

    create homepage views

--- CSEC_App/views.py
+++ CSEC_App/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import UpdateView,CreateView,DeleteView,ListView,DetailView
+from .models import Members
 
 # Create your views here.
+
+class HomeView(ListView):
+    model=Members
+    template_name='members_dashboard.html'
+    
+
+
