05a5ff7aae1c66648df99903bb0b44fbf7bf7e97
felekekinfe <felekekinfe@gmail.com>
2023-11-21 07:15:49 -0500

    add url route for each views

---
+++ CSEC_App/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import HomeView,AddMembersView,AddEventsView,EventView,EventDetailView
+
+urlpatterns = [
+    path('',home,name='home'),
+    path('add_member/',AddMembersView.as_view(),name='addmember'),
+    path('add_event',AddEventsView.as_view(),name='addevent'),
+    path('upcommingevent',EventView.as_view(),name='events'),
+    path('eventdetail/<int:pk>',EventDetailView.as_view(),name='eventdetail')
+
+    
+
+]
