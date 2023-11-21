dfd753235d34a61b15783076879020dc3dae21c1
felekekinfe <felekekinfe@gmail.com>
2023-11-21 06:26:44 -0500

    create AddMembersView

--- CSEC_App/views.py
+++ CSEC_App/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import UpdateView,CreateView,DeleteView,ListView,DetailView
 from .models import Members
+from .forms import MembersRegistrationForm
 
 # Create your views here.
 
 class HomeView(ListView):
     model=Members
     template_name='members_dashboard.html'
-    
+
+class AddMembersView(CreateView):
+    model=Members
+    form_class=MembersRegistrationForm
+    template_name='add_member.html'
+
 
 
