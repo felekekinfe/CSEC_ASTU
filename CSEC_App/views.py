4f1a6b647c258d8f323b8bf4ceb8f27805febe65
felekekinfe <felekekinfe@gmail.com>
2023-11-21 06:50:46 -0500

    create delete and detail views

--- CSEC_App/views.py
+++ CSEC_App/views.py
@@ -8,26 +8,32 @@ from .forms import MembersRegistrationForm
 class HomeView(ListView):
     model=Members
     template_name='members_dashboard.html'
-class EventsView(ListView):
-    model=Events
-    template_name='events_dashboard.html'
-class EventDetailView(DetailView):
-    model=Events
-    template_name='events_detail.html'
 
 class AddMembersView(CreateView):
     model=Members
     form_class=MembersRegistrationForm
     template_name='add_member.html'
 
+class DeleteMember(DeleteView):
+    model=Members
+    template_name='delete_member.html'
+
+class EventsView(ListView):
+    model=Events
+    template_name='events_dashboard.html'
+
+class EventDetailView(DetailView):
+    model=Events
+    template_name='events_detail.html'
+
 class AddEventsView(CreateView):
     model=Events
     template_name='add_events.html'
     fields='__all__'
 
-class DeleteMember(DeleteView):
-    model=Members
-    template_name='delete_member.html'
+class AddEventsView(DeleteView):
+    model=Events
+    template_name='delete_events.html'
     
 
 
