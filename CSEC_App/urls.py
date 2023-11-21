e93cd8f4bf8e8532cb5867a336e069bf7203a7c6
felekekinfe <felekekinfe@gmail.com>
2023-11-21 10:04:54 -0500

    add delete_events url route

--- CSEC_App/urls.py
+++ CSEC_App/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('add_event',AddEventsView.as_view(),name='addevent'),
     path('up_comming_event',EventsView.as_view(),name='events'),
     path('event_detail/<int:pk>',EventDetailView.as_view(),name='eventdetail'),
-    path('delete_member/<int:pk>',DeleteMember.as_view(),name='deletemember')
+    path('delete_member/<int:pk>',DeleteMember.as_view(),name='deletemember'),
+    path('delet_events/<int:pk>',DeleteEventsView.as_view(),name='deleteevents')
+
 
 
 
