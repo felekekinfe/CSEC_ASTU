7292274f73937371ac05ef99b417eeddd10b9340
felekekinfe <felekekinfe@gmail.com>
2023-11-21 10:02:55 -0500

    add delete_member url route

--- CSEC_App/urls.py
+++ CSEC_App/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import HomeView,AddMembersView,AddEventsView,EventsView,EventDetailView
+from .views import HomeView,AddMembersView,AddEventsView,EventsView,EventDetailView,DeleteMember,DeleteEventsView
 
 urlpatterns = [
     path('',HomeView.as_view(),name='homepage'),
     path('add_member/',AddMembersView.as_view(),name='addmember'),
     path('add_event',AddEventsView.as_view(),name='addevent'),
     path('up_comming_event',EventsView.as_view(),name='events'),
-    path('eventdetail/<int:pk>',EventDetailView.as_view(),name='eventdetail')
+    path('event_detail/<int:pk>',EventDetailView.as_view(),name='eventdetail'),
+    path('delete_member/<int:pk>',DeleteMember.as_view(),name='deletemember')
 
 
 
